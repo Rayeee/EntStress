d73242fc693d10e97f37b2a216d6cce980193643
zhugongyi <zhugongyi@niwodai.net>
2017-11-23 11:28:10 +0800

    fix

--- src/main/java/com/babel/stress/controller/PingController.java
+++ src/main/java/com/babel/stress/controller/PingController.java
@@ -1,10 +1,16 @@
 package com.babel.stress.controller;
 
+import com.xiaoleilu.hutool.io.IoUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by Rayee on 2017/11/23.
@@ -17,6 +23,13 @@ public class PingController {
     @RequestMapping(value = "/ping", method = RequestMethod.GET)
     public ResponseEntity<String> ping(){
         return ResponseEntity.ok("pong");
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/hook", method = RequestMethod.POST)
+    public ResponseEntity<String> hook(HttpServletRequest request) throws IOException {
+        String read = IoUtil.read(new BufferedReader(new InputStreamReader(request.getInputStream())));
+        return ResponseEntity.ok(read);
     }
 
 }
